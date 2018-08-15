1cafe7040e3de6c815a2f0e1f1ac4a498a2c0737
Alejandro Morelos <yaveth.morelos@gmail.com>
2018-08-15 08:24:32 -0500

    first commit

---
+++ ejemplo1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Todos los hombres son mortales" << endl;
+	cout << "Juan es hombre" << endl;
+	cout << "Todos somos Juan" << endl;
+	return 0;
+}
+
